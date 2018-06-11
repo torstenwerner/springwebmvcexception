9306358d275fdd503ee6e94b598fc63e140f07c8
Torsten Werner <torsten.werner@westernacher.com>
2018-06-11 21:14:55 +0200

    initial import

---
+++ src/main/java/com/example/springwebmvcexception/SpringwebmvcexceptionApplication.java
@@ -0,0 +1,12 @@
+package com.example.springwebmvcexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebmvcexceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebmvcexceptionApplication.class, args);
+    }
+}
